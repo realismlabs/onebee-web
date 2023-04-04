4c3f1f8120154cbbac9a4a40f8f235592459339f
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-04-04 16:13:40 -0400

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -85,7 +85,6 @@ export default function Home() {
             MozBackgroundClip: "text",
             backgroundClip: "text",
             color: "transparent",
-
             animation: "fadeInFromBottom 0.4s ease-in-out",
             animationDelay: "0.3s",
             animationFillMode: "forwards",
@@ -131,9 +130,17 @@ export default function Home() {
             </h2>
 
             <div className="mt-4">
-              <div className="inline-block bg-[#4315F3]">
+              <div
+                className="inline-block bg-[#4315F3] rounded-full"
+                style={{
+                  padding: "1px",
+                  background: "linear-gradient(to right, #8160FF, #4315F3)",
+                  animation: "rotate 3s linear infinite",
+                  backgroundSize: "400% 400%",
+                }}
+              >
                 <button
-                  className={`inline-block bg-[#4315F3] py-2 px-6 w-auto rounded-full hover:bg-[#3A16C7] hover:transition-all`}
+                  className={`bg-[#4315F3] py-2 px-6 w-auto rounded-full hover:bg-[#3A16C7] hover:transition-all`}
                 >
                   Launch playground
                   {/* <div
