be2965e3fc6e098081541ae74088022506dcb4b4
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-04-14 17:38:14 -0400

    Test

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -19,9 +19,7 @@ const AccountHeader: React.FC<AccountHeaderProps> = ({ email }) => {
   return (
     <div className="w-full flex flex-row h-16 items-center p-12 bg-slate-1">
       <div className="flex flex-col grow items-start">
-        <p className="text-sm text-gray-500 font-medium mb-0.5">
-          Logged in as:
-        </p>
+        <p className="text-sm text-slate-11 mb-1">Logged in as:</p>
         <p className="text-sm text-white font-medium">{email}</p>
       </div>
       <div className="flex flex-col grow items-end">
@@ -42,20 +40,22 @@ export default function Welcome() {
   const { user } = useUser();
   const email = user?.email;
 
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-
-  if (!email) {
-    return <div>Something went wrong</div>;
-  }
-
   return (
-    <>
-      <AccountHeader email={email} />
-      <>
-        <div className="bg-slate-1 text-white">Hello</div>
-      </>
-    </>
+    <div className="h-screen bg-slate-1">
+      <AccountHeader email={email ?? "placeholder"} />
+      <div className="flex flex-col justify-center items-center w-full pt-32">
+        <div className="bg-slate-1 text-white text-center text-2xl pb-4">
+          Welcome to Dataland, Arthur.
+        </div>
+        <div className="text-slate-11 max-w-md text-center text-lg pb-8">
+          Dataland makes it easy for your whole team to browse data from your
+          data warehouse.
+        </div>
+        <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 text-lg rounded-md">
+          Get started
+        </button>{" "}
+        .
+      </div>
+    </div>
   );
 }
