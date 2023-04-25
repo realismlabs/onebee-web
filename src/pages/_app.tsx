7cd46c476fb8315251d3381d321c27a935f36eb8
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-04-25 13:00:29 -0400

    Add persistQueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "../components/UserContext";
-import { QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../utils/queryClient";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 }
