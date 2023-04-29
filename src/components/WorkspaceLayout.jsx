da3b5c134f06f46d36b0a6206abf66789a22074c
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-04-29 15:22:20 -0400

    Get ugly commandbar starting to work

--- src/components/WorkspaceLayout.jsx
+++ src/components/WorkspaceLayout.jsx
@@ -1,11 +1,13 @@
 // components/Layout.js
 import React from 'react';
 import WorkspaceShell from './WorkspaceShell';
+import CommandBar from './CommandBar';
 
 const Layout = ({ children }) => {
   return (
     <div className="flex h-screen">
       <WorkspaceShell />
+      <CommandBar />
       <div className="flex-grow">{children}</div>
     </div>
   );
