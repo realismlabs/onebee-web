dc64db98d328147d77b5cdd0813a693ed9f968a0
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-04-30 15:16:08 -0400

    Update copy

--- src/components/CommandBar.jsx
+++ src/components/CommandBar.jsx
@@ -78,7 +78,7 @@ export const CommandBar = () => {
     },
     {
       name: 'Home',
-      description: '',
+      description: 'Go back home',
       icon: <House width={20} height={20} weight="fill" />,
       type: 'navigation',
       id: 'home',
@@ -117,7 +117,7 @@ export const CommandBar = () => {
           className="absolute inset-0  left-[50%] translate-x-[-50%] top-[25%] translate-y-[-25%] max-h-[40vh] overflow-y-auto bg-[#101112] text-white shadow-2xl text-[14px] rounded-xl data-[state=open]:animate-commandBar">
           <div className={`command-dialog-content ${open ? 'open' : ''}`}>
             <div className="sticky top-0 flex flex-col border-b border-slate-4 px-[16px] pt-[12px] bg-[#101112]">
-              <div cmdk-linear-badge="" className="text-[13px] text-slate-11 px-[6px] py-[3px] rounded-md bg-slate-2 w-fit">Home</div>
+              <div cmdk-linear-badge="" className="text-[13px] text-slate-11 px-[6px] py-[3px] rounded-md bg-slate-2 w-fit">Jump to:</div>
               <Command.Input autoFocus placeholder="Type a command or search..."
                 className="bg-[#101112] w-full border-none text-[14px] focus:outline-none focus:ring-0 px-[0px]"
               />
