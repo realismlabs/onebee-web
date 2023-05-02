6bd6bb97b89adff2d6256c1f926e2e5437a4b682
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-05-02 16:35:02 -0400

    Delete modal wip

---
+++ src/pages/workspace/[workspaceId]/table/[tableId].jsx
@@ -0,0 +1,394 @@
+import { useRouter } from "next/router";
+import { getTable, getConnection, updateTable } from "@/utils/api";
+import { assignColor } from "@/utils/util";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import { useCurrentWorkspace } from "@/hooks/useCurrentWorkspace";
+import WorkspaceLayout from "@/components/WorkspaceLayout";
+import MockTable from "@/components/MockTable";
+import LogoSnowflake from "@/components/LogoSnowflake";
+import LogoBigQuery from "@/components/LogoBigQuery";
+import LogoPostgres from "@/components/LogoPostgres";
+import { CaretDown, MagnifyingGlass, Gear, X } from "@phosphor-icons/react";
+import { useState, Fragment, useRef } from "react";
+import { IconLoaderFromSvgString } from "@/components/IconLoaderFromSvgString";
+import IconPickerPopoverInline from "@/components/IconPickerPopoverInline";
+import { Popover, Transition, Dialog } from "@headlessui/react";
+
+const TablePopover = ({
+  tableName,
+  tableId,
+  workspaceId,
+}) => {
+  const [isRenameDialogOpen, setIsRenameDialogOpen] = useState(false);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+
+  const openRenameDialog = () => {
+    setDisplayNameInputError("");
+    setDisplayNameInputValue(tableName);
+    setIsRenameDialogOpen(true);
+  };
+
+  const closeRenameDialog = () => {
+    setIsRenameDialogOpen(false);
+  };
+
+  const openDeleteDialog = () => {
+    setIsDeleteDialogOpen(true);
+  };
+
+  const closeDeleteDialog = () => {
+    setIsDeleteDialogOpen(false);
+  };
+
+
+  const displayNameInputRef = useRef(null);
+  const [selectedConnectionId, setSelectedConnectionId] = useState(null);
+  const [displayNameInputValue, setDisplayNameInputValue] = useState(tableName);
+  const [isEditingDisplayName, setIsEditingDisplayName] = useState(false);
+  const [displayNameInputError, setDisplayNameInputError] = useState("");
+
+  const handleUpdateDisplayName = async (e) => {
+    const new_table_name = e.target.value;
+    if (new_table_name === "") {
+      setDisplayNameInputError("Name cannot be empty");
+      return;
+    } else if (new_table_name === tableName) {
+      setIsEditingDisplayName(false);
+      closeRenameDialog();
+      return;
+    } else if (new_table_name.length > 64) {
+      setDisplayNameInputError("Name cannot be longer than 64 characters");
+      return;
+    } else {
+      setDisplayNameInputError("");
+    }
+    const tableData = {
+      displayName: new_table_name,
+    };
+    try {
+      await updateTableMutation.mutateAsync({ workspaceId, tableId, tableData });
+      setIsEditingDisplayName(false);
+      closeRenameDialog();
+    } catch (error) {
+      console.error('Error updating table:', error);
+    }
+  };
+
+  const queryClient = useQueryClient();
+
+  const updateTableMutation = useMutation(updateTable, {
+    onSuccess: async (updatedTable) => {
+      console.log("Table updated:", updatedTable);
+      await queryClient.refetchQueries([
+        "getTable",
+        workspaceId,
+        String(tableId),
+      ]);
+      await queryClient.refetchQueries(["workspaceTables", workspaceId]);
+    },
+    onError: (error) => {
+      console.error("Error updating table:", error);
+    },
+    invalidateQueries: [
+      ["getTable", workspaceId, tableId],
+      ["workspaceTables", workspaceId],
+    ],
+  });
+
+  return (
+    <>
+      <Popover className="relative">
+        {({ open }) => (
+          <>
+            <Popover.Button
+              className={`text-slate-11 hover:bg-slate-3 focus:shadow-slate-7 flex flex-row gap-[4px] px-[8px] py-[4px] items-center justify-center rounded-[4px] focus:outline-none
+              ${open ? "bg-slate-3" : "hover:bg-slate-3 active:bg-slate-4"}`}
+            >
+              <p className="text-slate-12 text-[13px]">{tableName}</p>
+              <CaretDown size={12} weight="bold" className="mt-[2px]" />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute z-10 w-[200px] max-w-[90vw] bg-slate-2 rounded-md shadow-slate-7 border border-slate-4 text-slate-12 focus:outline-none text-[13px]">
+                <div className="flex flex-col px-[8px] py-[8px] w-full text-[13px] text-slate-12">
+                  <>
+                    <Popover.Button className="w-full">
+                      <button
+                        className="hover:bg-slate-4 px-[8px] py-[6px] text-left flex flex-row gap-3 w-full rounded-md items-center"
+                        onClick={openRenameDialog}
+                      >
+                        Rename table
+                      </button>
+                    </Popover.Button>
+                    <Popover.Button className="w-full">
+                      <button className="hover:bg-slate-4 px-[8px] py-[6px] text-left flex flex-row gap-3 w-full rounded-md items-center"
+                        onClick={openDeleteDialog}
+                      >
+                        Delete table
+                      </button>
+                    </Popover.Button>
+                  </>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+      <Dialog
+        as="div"
+        open={isRenameDialogOpen}
+        onClose={() => setIsRenameDialogOpen(false)}
+        className="absolute z-20 inset-0 h-fit ml-[270px] mt-[40px]"
+      >
+        <Dialog.Panel>
+          <div className="flex flex-col bg-slate-3 w-fit rounded-[8px]">
+            <div className="p-[8px] rounded-[4px] text-[13px] flex flex-row items-center">
+              <input
+                title="Display name"
+                type="text"
+                value={displayNameInputValue}
+                onChange={(e) => setDisplayNameInputValue(e.target.value)}
+                className="border border-blue-700 rounded-md text-slate-12 bg-transparent py-[2px] px-[8px] text-[13px] w-[240px]"
+                ref={displayNameInputRef}
+                onBlur={handleUpdateDisplayName}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleUpdateDisplayName(e);
+                  }
+                  if (e.key === "Escape") {
+                    setDisplayNameInputValue(tableName || "");
+                    setIsEditingDisplayName(false);
+                  }
+                }}
+              />
+              <button
+                onClick={() => {
+                  setIsEditingDisplayName(false);
+                  closeRenameDialog();
+                }}
+                className="text-slate-10 hover:bg-slate-2 h-[20px] w-[20px] ml-[12px] flex items-center"
+              >
+                <X size={16} />
+              </button>
+            </div>
+            {displayNameInputError && (
+              <div className="text-red-9 px-[8px] pb-[4px] text-[12px]">
+                {displayNameInputError}
+              </div>
+            )}
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+      <Dialog
+        as="div"
+        open={isDeleteDialogOpen}
+        onClose={() => setIsDeleteDialogOpen(false)}
+        className="absolute inset-0 flex min-w-full h-screen"
+      >
+        <Dialog.Overlay>
+          <div className="fixed inset-0 bg-slate-1 opacity-50" />
+        </Dialog.Overlay>
+        <Dialog.Panel className="absolute z-30 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[400px]">
+          <div className="flex flex-col bg-slate-2 border border-slate-4 rounded-[8px] w-full p-[24px] text-slate-12">
+            {/* Close */}
+            <div className="rounded-[4px] text-[13px] absolute right-[16px] top-[16px] z-40">
+              <button
+                onClick={() => {
+                  closeDeleteDialog();
+                }}
+                className="text-slate-11 hover:bg-slate-4 rounded-md h-[24px] w-[24px] ml-[12px] flex items-center justify-center"
+              >
+                <X size={16} />
+              </button>
+            </div>
+            <Dialog.Title className="text-[16px]">Delete table</Dialog.Title>
+            <Dialog.Description className="text-[14px] mt-[16px]">Are you sure you want to delete this table? <br />This action is irreversible.</Dialog.Description>
+            <div className="font-mono w-full break-all bg-slate-3 text-slate-12 border border-slate-4 rounded-md font-medium text-[14px] px-[8px] py-[4px] mt-[12px]">{tableName}</div>
+            <div className="flex w-full justify-end mt-[16px] gap-2">
+              <button
+                className="ml-auto bg-slate-5 hover:bg-slate-6 border-slate-7 border text-[13px] text-slate-12 px-[12px] py-[4px] rounded-[4px]"
+                onClick={() => {
+                  closeDeleteDialog();
+                }
+                }
+              >
+                Cancel
+              </button>
+              <button
+                className="bg-red-5 hover:bg-red-6 border-red-7 border text-[13px] text-slate-12 px-[12px] py-[4px] rounded-[4px]"
+                onClick={() => {
+                  deleteConnectionMutation.mutate({
+                    workspaceId: currentWorkspace.id,
+                    connectionId: selectedConnectionId,
+                  })
+                  closeDeleteDialog();
+                }
+                }
+              >
+                Delete
+              </button>
+            </div>
+            {displayNameInputError && (
+              <div className="text-red-9 px-[8px] pb-[4px] text-[12px]">
+                {displayNameInputError}
+              </div>
+            )}
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </>
+  );
+};
+
+export default function TablePage() {
+  const router = useRouter();
+  const { tableId } = router.query;
+
+  console.log("id", tableId);
+
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const [isEditPopoverOpen, setIsEditPopoverOpen] = useState(false);
+  const [isRenamePopoverOpen, setIsRenamePopoverOpen] = useState(false);
+
+  const openRenamePopover = () => {
+    setIsRenamePopoverOpen(true);
+    console.log("open rename popover");
+    console.log("isRenamePopoverOpen", isRenamePopoverOpen);
+  };
+
+  const handleSearchbarFocus = () => {
+    setIsSearchFocused(true);
+  };
+
+  const handleSearchbarBlur = () => {
+    setIsSearchFocused(false);
+  };
+
+  const handleSearchbarKeyDown = (e) => {
+    if (e.key === "Escape") {
+      e.target.blur();
+    }
+  };
+
+  const {
+    data: currentUser,
+    isLoading: isUserLoading,
+    error: userError,
+  } = useCurrentUser();
+
+  const {
+    data: currentWorkspace,
+    isLoading: isWorkspaceLoading,
+    error: workspaceError,
+  } = useCurrentWorkspace();
+
+  const {
+    data: tableData,
+    isLoading: isTableLoading,
+    error: tableError,
+  } = useQuery({
+    queryKey: ["getTable", currentWorkspace?.id, tableId],
+    queryFn: async () => {
+      const response = await getTable(currentWorkspace?.id, tableId);
+      console.log("awu response", JSON.stringify(response));
+      return response;
+    },
+    enabled: currentWorkspace?.id !== null,
+  });
+
+  const {
+    data: connectionData,
+    isLoading: isConnectionLoading,
+    error: connectionError,
+  } = useQuery({
+    queryKey: ["getConnection", currentWorkspace?.id, tableData?.connectionId],
+    queryFn: async () => {
+      const response = await getConnection(
+        currentWorkspace.id,
+        tableData.connectionId
+      );
+      return response;
+    },
+    enabled: currentWorkspace?.id !== null && tableData?.connectionId !== null,
+  });
+
+  if (isTableLoading || isConnectionLoading) {
+    return (
+      <div className="h-screen bg-slate-1 text-slate-12 text-[11px] flex items-center justify-center">
+        Loading..
+      </div>
+    );
+  }
+
+  if (tableError || connectionError) {
+    return <div>There was an error loading your table</div>;
+  }
+
+  return (
+    <WorkspaceLayout>
+      <div className="bg-slate-1 max-h-screen text-slate-12 flex flex-col divide-slate-4 divide-y">
+        <div className="flex flex-row gap-2 items-center py-[12px] px-[12px] h-[48px]">
+          <div className="flex flex-row items-center justify-center">
+            <IconPickerPopoverInline
+              iconSvgString={tableData.iconSvgString}
+              tableName={tableData.displayName}
+              tableId={tableData.id}
+              workspaceId={currentWorkspace?.id}
+            />
+            <TablePopover
+              tableName={tableData.displayName}
+              tableId={tableData.id}
+              workspaceId={currentWorkspace?.id}
+            />
+          </div>
+          <div className="flex flex-row gap-2 ml-auto">
+            <div className="bg-slate-2 hover:bg-slate-3 text-[13px] px-[12px] py-[6px] border border-slate-4 cursor-pointer rounded-[6px] flex flex-row gap-1 items-center">
+              <p>Columns</p>
+              <CaretDown size={12} className="text-slate-11" />
+            </div>
+            <div
+              className={`w-[480px] bg-slate-2 hover:bg-slate-3 text-[13px] px-[6px] py-[6px] border border-slate-4 ${isSearchFocused ? "ring-2 ring-blue-600" : ""
+                } cursor-pointer rounded-[6px] flex flex-row gap-1 items-center`}
+            >
+              <MagnifyingGlass size={16} className="text-slate-11" />
+              <input
+                title="Search"
+                className="bg-transparent focus:outline-none focus:ring-0 placeholder:text-slate-10"
+                placeholder="Search.."
+                onFocus={handleSearchbarFocus}
+                onBlur={handleSearchbarBlur}
+                onKeyDown={handleSearchbarKeyDown}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="grow-1 overflow-x-auto overflow-y-scroll max-w-screen">
+          <MockTable />
+        </div>
+        <div className="flex flex-row gap-2 items-center border-b border-slate-4 px-[20px] py-[8px] text-[13px] text-slate-11">
+          <div>Full path</div>
+
+          <div>{tableData.fullName}</div>
+          <div>from</div>
+          <div className="flex flex-row gap-1 items-center">
+            {" "}
+            {connectionData.connectionType === "snowflake" && (
+              <div className="h-[16px] w-[16px]">
+                <LogoSnowflake />
+              </div>
+            )}
+            {connectionData.name}
+          </div>
+        </div>
+      </div>
+    </WorkspaceLayout>
+  );
+}
