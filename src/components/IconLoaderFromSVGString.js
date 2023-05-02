9daa9aa21949700e471adf99c41d55d5aae872e4
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-05-02 17:12:10 -0400

    Fix build issus again?

--- src/components/IconLoaderFromSVGString.js
+++ src/components/IconLoaderFromSVGString.js
@@ -2,10 +2,12 @@ import parse from 'html-react-parser';
 import { Table } from '@phosphor-icons/react'
 import { assignColor } from '@/utils/util';
 
-export const IconLoaderFromSvgString = ({ iconSvgString, tableName }) => {
+const IconLoaderFromSvgString = ({ iconSvgString, tableName }) => {
   if (typeof iconSvgString === "string") {
     return <div className="flex items-center justify-center h-[20px] w-[20px] rounded cursor-pointer">{parse(iconSvgString)}</div>;
   } else {
     return <Table size={20} weight="fill" color={assignColor(tableName)} />
   }
 }
+
+export default IconLoaderFromSvgString;
