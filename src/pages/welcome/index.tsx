85076f19623f59be9c9164d95597e5e67f54a032
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-05-10 16:47:45 -0400

    tron

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import Link from "next/link";
 import router from "next/router";
 import { useCurrentUser } from "../../hooks/useCurrentUser";
+import { motion } from "framer-motion";
 
 interface AccountHeaderProps {
   email: string;
@@ -134,6 +135,20 @@ function generateCircleShadows(
   return circles;
 }
 
+function circuitPath(path: string) {
+  return (
+    <path fill="none" stroke="var(--slate3)" strokeWidth={0.2} d={path}>
+      <animate
+        attributeName="opacity"
+        from="1"
+        to="0"
+        dur="1.0s"
+        fill="freeze"
+        begin="2s"
+      />
+    </path>
+  );
+}
 const CometAnimation: React.FC = () => {
   const pathl1 = "M20,100 L20,90 L90,90 L90,80";
   const pathl2 = "M22,100 L22,92 L92,92 L92,80";
@@ -143,7 +158,8 @@ const CometAnimation: React.FC = () => {
   const pathr2 = "M178,100 L178,92 L108,92 L108,80";
   const pathr3 = "M176,100 L176,94 L106,94 L106,80";
 
-  const screenborder = "M100,80 L180,80 L180,10 L20,10 L20,80 z";
+  const screenborder = "M100,80 L180,80 L180,15 L20,15 L20,80 z";
+  const screenborderOpposite = "M100,80 L20,80 L20,15 L180,15 L180,80 z";
 
   return (
     <div
@@ -151,6 +167,7 @@ const CometAnimation: React.FC = () => {
       style={{
         background: "var(--slate-1)",
         zIndex: -1,
+        transform: "rotate(180deg)",
       }}
     >
       <div className="min-w-[1400px] h-screen flex justify-end">
@@ -161,83 +178,36 @@ const CometAnimation: React.FC = () => {
         >
           <path
             fill="none"
-            stroke="var(--slate4)"
+            stroke="var(--slate3)"
             strokeWidth={0.2}
             d={screenborder}
           />
-          {generateCircles(20, screenborder, 2, 10, "indefinite")}
+          {generateCircles(12, screenborder, 2, 10, "indefinite")}
           {generateCircleShadows(3, screenborder, 2, 10, "indefinite")}
-          <path
-            fill="none"
-            stroke="var(--slate4)"
-            strokeWidth={0.2}
-            d={pathl1}
-          />
-          {generateCircles(20, pathl1)}
+          {generateCircles(12, screenborderOpposite, 2, 10, "indefinite")}
+          {generateCircleShadows(3, screenborderOpposite, 2, 10, "indefinite")}
+          {circuitPath(pathl1)}
+          {generateCircles(6, pathl1)}
           {generateCircleShadows(3, pathl1)}
-          <path
-            fill="none"
-            stroke="var(--slate4)"
-            strokeWidth={0.2}
-            d={pathl2}
-          />
-          {generateCircles(20, pathl2)}
+          {circuitPath(pathl2)}
+          {generateCircles(6, pathl2)}
           {generateCircleShadows(3, pathl2)}
-          <path
-            fill="none"
-            stroke="var(--slate4)"
-            strokeWidth={0.2}
-            d={pathl3}
-          />
-          {generateCircles(20, pathl3)}
+          {circuitPath(pathl3)}
+          {generateCircles(6, pathl3)}
           {generateCircleShadows(3, pathl3)}
-          <path
-            fill="none"
-            stroke="var(--slate4)"
-            strokeWidth={0.2}
-            d={pathr1}
-          />
-          {generateCircles(20, pathr1)}
+          {circuitPath(pathr1)}
+          {generateCircles(6, pathr1)}
           {generateCircleShadows(3, pathr1)}
-          <path
-            fill="none"
-            stroke="var(--slate4)"
-            strokeWidth={0.2}
-            d={pathr2}
-          />
-          {generateCircles(20, pathr2)}
+          {circuitPath(pathr2)}
+          {generateCircles(6, pathr2)}
           {generateCircleShadows(3, pathr2)}
-          <path
-            fill="none"
-            stroke="var(--slate4)"
-            strokeWidth={0.2}
-            d={pathr3}
-          />
-          {generateCircles(20, pathr3)}
+          {circuitPath(pathr3)}
+          {generateCircles(6, pathr3)}
           {generateCircleShadows(3, pathr3)}
           <ellipse
             cx="100"
             cy="80"
-            rx="50"
-            ry="0.2"
-            opacity="0"
-            fill="#DBFFFF"
-            style={{
-              mixBlendMode: "overlay",
-            }}
-          >
-            <animate
-              attributeName="opacity"
-              values="0;1;0"
-              dur="1.0s"
-              begin="2s"
-              // repeatCount="indefinite"
-            />
-          </ellipse>
-          <ellipse
-            cx="100"
-            cy="80"
-            rx="50"
+            rx="75"
             ry="1"
             opacity="0"
             fill="#0085FF"
@@ -251,9 +221,59 @@ const CometAnimation: React.FC = () => {
               values="0;1;0"
               dur="1.0s"
               begin="2s"
-              // repeatCount="indefinite"
             />
           </ellipse>
+          <ellipse
+            cx="100"
+            cy="80"
+            rx="75"
+            ry="0.05"
+            opacity="0"
+            fill="#DBFFFF"
+            style={{
+              mixBlendMode: "normal",
+            }}
+          >
+            <animate
+              attributeName="opacity"
+              values="0;1;0"
+              dur="1.0s"
+              begin="2s"
+            />
+          </ellipse>
+          {/* const screenborder = "M100,80 L180,80 L180,15 L20,15 L20,80 z"; */}
+          <path
+            d="M20,80 Q100,70, 180,80 L180,80 L70,80 Z"
+            fill="#0085FF"
+            opacity="0"
+            style={{
+              mixBlendMode: "overlay",
+              filter: "blur(10px)",
+            }}
+          >
+            <animate
+              attributeName="opacity"
+              values="0;0.5;0"
+              dur="1.0s"
+              begin="2s"
+            />
+          </path>
+          <path
+            d="M60,80 Q100,70, 140,80 L140,80 L70,80 Z"
+            fill="#DBFFFF"
+            opacity="0"
+            style={{
+              mixBlendMode: "overlay",
+              filter: "blur(10px)",
+            }}
+          >
+            <animate
+              attributeName="opacity"
+              values="0;0.5;0"
+              dur="1.0s"
+              begin="2s"
+            />
+          </path>
         </svg>
       </div>
     </div>
@@ -279,24 +299,44 @@ export default function Welcome() {
 
   return (
     <div className="h-screen bg-slate-1 z-10 relative">
-      <CometAnimation />
-      <AccountHeader email={email ?? "placeholder@example.com"} />
-      <div className="flex flex-col justify-center items-center w-full pt-32 z-20">
-        <div className="text-slate-12 text-center text-[22px] pb-4">
-          Welcome to Dataland, Arthur.
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <CometAnimation />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 2.2 }}
+      >
+        <AccountHeader email={email ?? "placeholder@example.com"} />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 2.2 }}
+      >
+        <div className="absolute inset-0 flex flex-col justify-center items-center h-screen">
+          <div className="flex flex-col justify-center items-center">
+            <div className="text-slate-12 text-center text-[22px] pb-4">
+              Welcome to Dataland, Arthur.
+            </div>
+            <div className="text-slate-11 max-w-md text-center text-lg pb-8">
+              Dataland makes it easy for your whole team to browse data from
+              your data warehouse.
+            </div>
+            <button
+              type="button"
+              className="bg-blue-600 hover:bg-blue-700 text-slate-12 text-[14px] font-medium py-2 px-4 rounded-md"
+              onClick={handleSubmit}
+            >
+              Get started
+            </button>
+          </div>
         </div>
-        <div className="text-slate-11 max-w-md text-center text-lg pb-8">
-          Dataland makes it easy for your whole team to browse data from your
-          data warehouse.
-        </div>
-        <button
-          type="button"
-          className="bg-blue-600 hover:bg-blue-700 text-slate-12 text-[14px] font-medium py-2 px-4 rounded-md"
-          onClick={handleSubmit}
-        >
-          Get started
-        </button>
-      </div>
+      </motion.div>
     </div>
   );
 }
