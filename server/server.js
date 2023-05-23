1eaf61cb3f6b5b678a2efc3d67644237af255e9a
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-05-22 21:33:05 -0400

    Update - working with node pg

--- server/server.js
+++ server/server.js
@@ -2,8 +2,28 @@ require('dotenv').config(); // To read CLERK_API_KEY
 const express = require('express');
 const { ClerkExpressRequireAuth } = require('@clerk/clerk-sdk-node');
 
+
 const port = process.env.PORT || 5002;
 const app = express();
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'postgres',
+  host: 'db.htnkbjjakwynjxocwsll.supabase.co',
+  database: 'postgres',
+  password: '7mUMCMFCAw4Cc2bc',
+  port: 6543,
+});
+
+app.get('/users', ClerkExpressRequireAuth(), async (req, res) => {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT * FROM users');
+    res.send(result.rows);
+  } finally {
+    client.release();
+  }
+});
 
 // Use the strict middleware that raises an error when unauthenticated
 app.get(
