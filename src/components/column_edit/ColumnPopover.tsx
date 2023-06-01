0a9bdca4fc795d6495693665f4481a418c16bfbe
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-06-01 18:00:23 -0400

    Update popover code

--- src/components/column_edit/ColumnPopover.tsx
+++ src/components/column_edit/ColumnPopover.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Popover, Transition } from "@headlessui/react";
+import { motion, AnimatePresence } from "framer-motion";
+import { CaretDown, CaretUp } from "@phosphor-icons/react";
+import useMeasure from "react-use-measure";
 
 interface Column {
   id: number;
@@ -9,15 +12,64 @@ interface Column {
   display_config: any;
 }
 
+const columnDisplayTypes = [
+  {
+    data_type: "number",
+    display_type: "plain_number",
+  },
+  {
+    data_type: "number",
+    display_type: "conditional_color_number",
+  },
+  {
+    data_type: "string",
+    display_type: "plain_text",
+  },
+  {
+    data_type: "string",
+    display_type: "json",
+  },
+  {
+    data_type: "string",
+    display_type: "color_coded_text",
+  },
+  {
+    data_type: "string",
+    display_type: "url",
+  },
+  {
+    data_type: "datetime",
+    display_type: "utc_datetime",
+  },
+  {
+    data_type: "datetime",
+    display_type: "local_datetime",
+  },
+  {
+    data_type: "boolean",
+    display_type: "plain_boolean",
+  },
+  {
+    data_type: "boolean",
+    display_type: "checkbox",
+  },
+];
+
 const ColumnPopover = (column: Column) => {
+  let [ref, bounds] = useMeasure();
+  const [isSelectingDisplayType, setIsSelectingDisplayType] =
+    React.useState(false);
+
+  const [displayType, setDisplayType] = React.useState(column.display_type);
+
   return (
     <Popover className="flex flex-col">
-      {({ open }) => (
+      {({ open, close }) => (
         <>
           <Popover.Button
             className={`
-              ${open ? "bg-slate-4" : "hover:bg-slate-5 active:bg-slate-6"}
-              flex flex-col gap-4 rounded-lg cursor-default px-3 py-1.5 text-[13px]`}
+              ${open ? "" : "hover:bg-slate-5 active:bg-slate-6"}
+              bg-slate-4 flex flex-col gap-4 rounded-lg cursor-default px-3 py-1.5 text-[13px]`}
           >
             Edit column
           </Popover.Button>
@@ -29,8 +81,89 @@ const ColumnPopover = (column: Column) => {
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <Popover.Panel className="absolute z-10 mt-3  max-h-[80vh] top-0 rounded-md bg-slate-2 shadow-2xl border border-slate-4 flex flex-shrink w-[260px] flex-col items-start justify-start">
-              <p> Hello!</p>
+            <Popover.Panel className="absolute z-10 mt-3 px-4 py-3 max-h-[80vh] top-0 rounded-md bg-slate-2 shadow-2xl border border-slate-4 flex flex-shrink w-[260px] flex-col gap-2 items-start justify-start cursor-default">
+              <p className="text-[13px] font-medium">Edit display type</p>
+              <motion.div
+                className="w-full mt-2"
+                animate={{ height: bounds.height }}
+                transition={{ duration: 0.2 }}
+              >
+                <div ref={ref} className="flex flex-col">
+                  <AnimatePresence mode="popLayout">
+                    {isSelectingDisplayType == false && (
+                      <div
+                        className="flex flex-row items-center flex-grow bg-slate-3  hover:bg-slate-4 active:bg-slate-5 py-2 px-4 rounded-md border border-slate-6"
+                        onClick={() => setIsSelectingDisplayType(true)}
+                      >
+                        <p className="text-[13px] mr-auto">{displayType}</p>
+                        <CaretDown
+                          size={12}
+                          className="text-slate-12"
+                          weight="bold"
+                        />
+                      </div>
+                    )}
+                    {isSelectingDisplayType == true && (
+                      <div className="flex-col flex bg-slate-3 rounded-md border border-slate-6">
+                        <div
+                          className="flex flex-row items-center flex-grow bg-slate-3  hover:bg-slate-4 active:bg-slate-5 py-2 px-4 rounded-md"
+                          onClick={() => {
+                            setIsSelectingDisplayType(false);
+                          }}
+                        >
+                          <p className="text-[13px] text-slate-11 mr-auto">
+                            Choose a display type..
+                          </p>
+                          <CaretUp
+                            size={12}
+                            className="text-slate-12"
+                            weight="bold"
+                          />
+                        </div>
+                        {/* map columnDisplayTypes but only show the display_types where it shares the same data_type */}
+                        {columnDisplayTypes
+                          .filter(
+                            (displayType) =>
+                              displayType.data_type === column.type
+                          )
+                          .map((displayType) => (
+                            <div
+                              className="flex flex-row items-center flex-grow bg-slate-3 hover:bg-slate-4 active:bg-slate-5 py-2 px-4 rounded-md"
+                              key={displayType.display_type}
+                              onClick={() => {
+                                setDisplayType(displayType.display_type);
+                                setIsSelectingDisplayType(false);
+                              }}
+                            >
+                              <p className="text-[13px] mr-auto">
+                                {displayType.display_type}
+                              </p>
+                            </div>
+                          ))}
+                      </div>
+                    )}
+                    <div className="flex flex-row gap-2 mt-4">
+                      <div
+                        className="bg-slate-3 hover:bg-slate-4 px-3 py-1.5 text-[13px] rounded-md ml-auto"
+                        onClick={() => {
+                          close();
+                        }}
+                      >
+                        Cancel
+                      </div>
+                      <div
+                        className="bg-blue-600 hover:bg-blue-700  px-3 py-1.5 text-[13px] rounded-md "
+                        onClick={() => {
+                          // run whatever validations here
+                          close();
+                        }}
+                      >
+                        Save
+                      </div>
+                    </div>
+                  </AnimatePresence>
+                </div>
+              </motion.div>
             </Popover.Panel>
           </Transition>
         </>
