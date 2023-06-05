04f150af50494b02111eaa53467e23fb9c40633d
Arthur Wu <5306113+arthurwuhoo@users.noreply.github.com>
2023-06-05 10:47:57 -0400

    Add fake types for react-email

---
+++ react-email-starter/types/react-email.d.ts
@@ -0,0 +1,5 @@
+// The @react-email/components doesn't seem to provide TypeScript types.
+// If you're not heavily relying on TypeScript for this particular module,
+// you can declare it in a .d.ts file in your project to suppress the error. Create a new file in your project
+// (for example react-email.d.ts), and add the following:
+declare module "@react-email/components";
